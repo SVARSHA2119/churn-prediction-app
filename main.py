f251d91a641e3346fbfdc40b9f0200f9f1a23c84
Varsha S <skvarsha2119@gmail.com>
2026-04-23 23:00:46 +0530

    updated backend logic with stay/leave + probability

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from predict import predict
-
-app = FastAPI()
-
-class Customer(BaseModel):
-    tenure: int
-    monthly_charges: float
-    total_charges: float
-
-@app.get("/health")
-def health():
-    return {"status": "OK"}
-
 @app.post("/predict")
 def get_prediction(data: Customer):
     features = [
